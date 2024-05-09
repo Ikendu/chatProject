ab31234d057f7ce454c2ab0d7a7dbff1cfebec43
Aniede Chibundu <ndubest56@gmail.com>
2024-05-09 20:23:21 +0100

    checking files

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const http = require("http");
 
 const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello Server</h1><h3>We are ready to work here</h3>");
-});
+app.use(express.static("client"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/client/index.html");
+// });
 
 const port = 3000;
 server.listen(port, () => console.log(`Listening at port ${port}`));
