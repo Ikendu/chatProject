6bbfa0334844bc7bbe2255c6c10939e00ff0124a
Aniede Chibundu <ndubest56@gmail.com>
2024-05-10 18:45:42 +0100

    create connect and disconnect a user

--- client/script.js
+++ client/script.js
@@ -1,4 +1,36 @@
 const socket = io();
+
+const message = document.getElementById("messages");
+const form = document.getElementById("grpMsgForm");
+const input = document.getElementById("grpMsgInput");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  if (input.value) {
+    const msg = input.value;
+    socket.emit("group message", msg);
+    input.value = "";
+  }
+};
+
+const messaging = (message, msg) => {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  message.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+};
+
 socket.on("connect", () => {
   console.log(`a client user with ${socket.id} connected`);
+  socket.on("new user", (id) => {
+    alert(`A new user with id ${id} has joined`);
+  });
+
+  socket.on("group message", (msg) => {
+    messaging(message, msg);
+  });
+
+  socket.on("user disconnected", (id) => {
+    alert(`User with ${id} has disconnected`);
+  });
 });
--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const app = express();
 const http = require("http");
 
 const server = http.createServer(app);
- 
+
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static("client"));
+
 io.on("connection", (socket) => {
   console.log(`A new user connected with ${socket.id}`);
-});
+  socket.broadcast.emit("new user", socket.id);
 
-app.use(express.static("client"));
+  socket.on("group message", (msg) => {
+    console.log(msg);
+    io.emit("group message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User with ${socket.id} has disconnected`);
+    socket.broadcast.emit("user disconnected", socket.id);
+  });
+});
 
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname + "/client/index.html");
