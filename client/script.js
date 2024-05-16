c429bebb7e601b2b0485f4906ae0476fe51733e3
Aniede Chibundu <ndubest56@gmail.com>
2024-05-16 09:20:09 +0100

    group message sending

--- client/script.js
+++ client/script.js
@@ -4,10 +4,18 @@ const messages = document.getElementById("messages");
 const input = document.getElementById("grpMsgInput");
 const form = document.getElementById("grpMsgForm");
 
+//display message for all users
+const messaging = (msgArea, msg) => {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  msgArea.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+};
+//create socket.io connection
 socket.on("connect", () => {
   console.log(`connected with ${socket.id}`);
   socket.emit("checker", "Ikendu");
-
+//send group message to server
   form.onsubmit = (e) => {
     e.preventDefault();
 
@@ -17,8 +25,8 @@ socket.on("connect", () => {
       input.value = "";
     }
   };
-
+  //display message for all users
   socket.on("group message", (msg) => {
-    alert(msg);
+    messaging(messages, msg);
   });
 });
