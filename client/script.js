2b10e503835ab6d7f6d3a3b3c86958f3fac10732
Aniede Chibundu <ndubest56@gmail.com>
2024-05-20 08:32:38 +0100

    general update

--- client/script.js
+++ client/script.js
@@ -1,4 +1,5 @@
 const socket = io();
+const usersTalkingPrivately = {};
 
 const messages = document.getElementById("messages");
 const input = document.getElementById("grpMsgInput");
@@ -7,15 +8,16 @@ const form = document.getElementById("grpMsgForm");
 socket.nickname = prompt("Enter you nickname");
 
 const newConnection = (nickname) => {
-  const newuser = document.getElementById("newUser");
+  const newuser = document.getElementById("connectionStatus");
   newuser.innerText = `Welcome ${nickname}`;
 };
 
 //display message for all users
-const messaging = (msgArea, msg) => {
+const messaging = (msgArea, name, msg) => {
   const item = document.createElement("li");
-  item.textContent = msg;
+  item.textContent = `${name}: ${msg}`;
   msgArea.appendChild(item);
+
   window.scrollTo(0, document.body.scrollHeight);
 };
 
@@ -30,21 +32,44 @@ socket.on("connect", () => {
     alert(`${user} has has joined the chat`);
   });
 
-  //send group message to server
-  form.onsubmit = (e) => {
-    e.preventDefault();
-    if (input.value) {
-      const msg = input.value;
-      socket.emit("group message", msg);
-      input.value = "";
-    }
-  };
-  //display message for all users
-  socket.on("group message", (msg) => {
-    messaging(messages, msg);
-  });
+  socket.on("users list", (listOfUsers) => {
+    const connectedUsers = document.getElementById("connectedUsers");
+    connectedUsers.innerHTML = "";
+    for (const id in listOfUsers) {
+      const nickname = listOfUsers[id];
+      const user = document.createElement("li");
+      user.innerText = nickname;
+      connectedUsers.appendChild(user);
 
-  socket.on("user disconnect", (user) => {
-    alert(`A user with id ${user} has disconnected`);
-  });
-});
+      //display private message button
+      const sendPrvMsg = document.createElement("button");
+      sendPrvMsg.innerText = "Send Message";
+      const yourSelf = document.createElement("span");
+      yourSelf.innerText = "Yourself";
+      if (id === socket.id) {
+        connectedUsers.appendChild(yourSelf);
+      } else {
+        connectedUsers.appendChild(sendPrvMsg);
+      }
+
+      //create private chat
+      const privateMsgs = document.getElementById("privateMsgs");
+      sendPrvMsg.onclick = () => {
+        if (!usersTalkingPrivately[id]) {
+          usersTalkingPrivately[id] = nickname;
+
+          //create mesg area
+          const privMsgArea = document.createElement("ul");
+          privMsgArea.id = id;
+          privMsgArea.classList.add("privMsg");
+
+          //create heading for each message
+          const privHeading = document.createElement("h1");
+          privHeading.innerText = `Mesages between you and ${nickname}`;
+
+          //create input area
+          const input = document.createElement("input");
+          //create button
+          const sendBtn = document.createElement("button");
+          sendBtn.type = "submit";
+          sendBtn.innerText = "Send";
